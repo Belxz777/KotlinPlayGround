8eef3d99e3f3330b9fbfec162da0ba2224240ef1
Roman <140193303+Belxz777@users.noreply.github.com>
2023-09-09 17:30:33 +0400

    full course kotlin examples (kidding)

--- main.kt
+++ main.kt
@@ -116,4 +116,54 @@ class   RealFunInClass{
 }
 var funRun = RealFunInClass("Name",32)
 funRun.printName()
+//что бы опредилить наследуемый класс используем конструкцию open
+open class OterClass(var name: String, var age: Int) {
+}
+//модификаторы видимости 
+//public  видимость везде
+//protected видимость не дальше одно наследоваемого класса ну подкласса
+//private видно только внутри самого класса 
+abstract class Useless ( var name:String,var ages:Int){
+   abstract fun FreeToExtend ()
+}
+class Admin(name: String, ages: Int): Useless(name, ages) {
+ override  fun FreeToExtend () {
+println("You name is " + name + "you are " +  ages+ "years old")
+ }
+}
+val admin  = Admin("Roman",10)
+ admin.FreeToExtend()
+ /*
+ Music Player
+You are building a Music Player app.
+You need to implement the MusicPlayer class, which should hold the track names as Strings in an array. The array is already defined in the given code.
+The player should support the following functions:
+add: add the given argument track to the tracks array.
+show: output all track names in the player on separate lines.
+play: start playing the first track by outputting "Playing name" where name is the first track name.
+
+You can add a new item to an array using +=, for example: tracks += track
+  */
+  //решение
+  class MusicPlayer {
+    private var songs: Array <String> = arrayOf()
+    
+   fun add(input:String){
+        songs+=input
+    }
+  fun show(){
+    for (i in songs){
+        println(i)
+}}
+  fun play(){
+    var name = songs[0]
+    println("Playing " + name)
+}
+    
+}
+var input = "i EAT deda"
+    val m = MusicPlayer()  
+        m.add(input)
+    m.show()
+    m.play()
 }
